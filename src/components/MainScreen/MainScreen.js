081fab81daf0f4ef504705d9699788d227f34df4
Ivan Harnouski <underclassity@gmail.com>
2023-08-24 22:04:19 +0300

    Add grid planes by params

--- src/components/MainScreen/MainScreen.js
+++ src/components/MainScreen/MainScreen.js
@@ -8,6 +8,12 @@ export default {
       size: 0.2,
       speed: 1,
 
+      pitWidth: 5,
+      pitHeight: 5,
+      pitDepth: 12,
+
+      isPause: false,
+
       camera: undefined,
       scene: undefined,
       renderer: undefined,
@@ -15,7 +21,7 @@ export default {
   },
 
   methods: {
-    generateMeshPoint() {
+    generateMeshPoint(line = true) {
       const { size } = this;
 
       const geometry = new THREE.BoxGeometry(size, size, size);
@@ -23,12 +29,30 @@ export default {
 
       const mesh = new THREE.Mesh(geometry, material);
 
-      return mesh;
+      if (!line) {
+        return mesh;
+      }
+
+      const group = new THREE.Group();
+
+      const wireframe = new THREE.WireframeGeometry(mesh.geometry);
+      const lineFrame = new THREE.LineSegments(wireframe);
+      lineFrame.material.depthTest = false;
+      lineFrame.material.opacity = 0.25;
+      lineFrame.material.transparent = true;
+
+      group.add(mesh);
+      group.add(lineFrame);
+
+      return group;
     },
 
     generateOnePoint() {
       const pointGroup = new THREE.Group();
-      pointGroup.add(this.generateMeshPoint());
+
+      const mesh = this.generateMeshPoint();
+
+      pointGroup.add(mesh);
 
       return pointGroup;
     },
@@ -192,6 +216,59 @@ export default {
       this.renderer.setSize(containerRect.width, containerRect.height);
     },
 
+    ToQuads(g) {
+      const p = g.parameters;
+      const segmentsX =
+        (g.type == "TorusBufferGeometry"
+          ? p.tubularSegments
+          : p.radialSegments) ||
+        p.widthSegments ||
+        p.thetaSegments ||
+        p.points.length - 1 ||
+        1;
+      const segmentsY =
+        (g.type == "TorusBufferGeometry"
+          ? p.radialSegments
+          : p.tubularSegments) ||
+        p.heightSegments ||
+        p.phiSegments ||
+        p.segments ||
+        1;
+      const indices = [];
+      for (let i = 0; i < segmentsY + 1; i++) {
+        let index11 = 0;
+        let index12 = 0;
+        for (let j = 0; j < segmentsX; j++) {
+          index11 = (segmentsX + 1) * i + j;
+          index12 = index11 + 1;
+          const index21 = index11;
+          const index22 = index11 + (segmentsX + 1);
+          indices.push(index11, index12);
+          if (index22 < (segmentsX + 1) * (segmentsY + 1) - 1) {
+            indices.push(index21, index22);
+          }
+        }
+        if (index12 + segmentsX + 1 <= (segmentsX + 1) * (segmentsY + 1) - 1) {
+          indices.push(index12, index12 + segmentsX + 1);
+        }
+      }
+      g.setIndex(indices);
+    },
+
+    generateGrid(width = 10, height = 10, color = 0x808080) {
+      const group = new THREE.Group();
+
+      const gXY = new THREE.PlaneGeometry(width, height, width, height);
+      this.ToQuads(gXY);
+      const mXY = new THREE.LineBasicMaterial({ color });
+
+      const grXY = new THREE.LineSegments(gXY, mXY);
+
+      group.add(grXY);
+
+      return grXY;
+    },
+
     init() {
       const { container } = this.$refs;
 
@@ -200,36 +277,63 @@ export default {
 
       const clock = new THREE.Clock();
 
-      const camera = new THREE.PerspectiveCamera(110, width / height, 0.01, 10);
-      camera.position.z = 3;
+      const camera = new THREE.PerspectiveCamera(
+        110,
+        width / height,
+        0.01,
+        100
+      );
+      camera.position.z = 1.5;
       this.camera = camera;
 
       const scene = new THREE.Scene();
       this.scene = scene;
 
-      const bottomPlane = new THREE.GridHelper(10, 10, 0x0000ff, 0x808080);
-      bottomPlane.rotateX(Math.PI / 2);
-      bottomPlane.position.z = -5;
-      scene.add(bottomPlane);
+      const bottomPlane = this.generateGrid(
+        this.pitWidth,
+        this.pitHeight,
+        0x808080
+      );
+      bottomPlane.position.z = -this.pitDepth;
 
-      const downPlane = new THREE.GridHelper(10, 10, 0x0000ff, 0x808080);
-      downPlane.rotateZ(Math.PI);
-      downPlane.position.y = -5;
-      scene.add(downPlane);
+      const downPlane = this.generateGrid(
+        this.pitWidth,
+        this.pitDepth,
+        0x808080
+      );
+      downPlane.rotateX(Math.PI / 2);
+      downPlane.position.z = -this.pitDepth / 2;
+      downPlane.position.y = -this.pitHeight / 2;
 
-      const upPlane = new THREE.GridHelper(10, 10, 0x0000ff, 0x808080);
-      upPlane.rotateZ(Math.PI);
-      upPlane.position.y = 5;
-      scene.add(upPlane);
+      const upPlane = this.generateGrid(this.pitWidth, this.pitDepth, 0x808080);
+      upPlane.rotateX(Math.PI / 2);
+      upPlane.position.z = -this.pitDepth / 2;
+      upPlane.position.y = this.pitHeight / 2;
 
-      const leftPlane = new THREE.GridHelper(10, 10, 0x0000ff, 0x808080);
+      const leftPlane = this.generateGrid(
+        this.pitHeight,
+        this.pitDepth,
+        0x808080
+      );
+      leftPlane.rotateY(Math.PI / 2);
       leftPlane.rotateZ(Math.PI / 2);
-      leftPlane.position.x = -5;
-      scene.add(leftPlane);
+      leftPlane.position.z = -this.pitDepth / 2;
+      leftPlane.position.x = -this.pitWidth / 2;
 
-      const rightPlane = new THREE.GridHelper(10, 10, 0x0000ff, 0x808080);
+      const rightPlane = this.generateGrid(
+        this.pitHeight,
+        this.pitDepth,
+        0x808080
+      );
+      rightPlane.rotateY(Math.PI / 2);
       rightPlane.rotateZ(Math.PI / 2);
-      rightPlane.position.x = 5;
+      rightPlane.position.z = -this.pitDepth / 2;
+      rightPlane.position.x = this.pitWidth / 2;
+
+      scene.add(bottomPlane);
+      scene.add(downPlane);
+      scene.add(upPlane);
+      scene.add(leftPlane);
       scene.add(rightPlane);
 
       const onePoint = this.generateOnePoint();
@@ -270,23 +374,27 @@ export default {
       const animation = (time) => {
         const delta = clock.getDelta();
 
+        if (this.isPause) {
+          return false;
+        }
+
         timeDelta += delta;
 
         const second = Math.round(timeDelta) * this.speed;
 
-        let changePosition = false;
+        // let changePosition = false;
 
-        if (second != prevTime) {
-          prevTime = second;
+        // if (second != prevTime) {
+        //   prevTime = second;
 
-          if (count > 3) {
-            count = -4;
-          }
+        //   if (count > 3) {
+        //     count = -4;
+        //   }
 
-          changePosition = true;
+        //   changePosition = true;
 
-          count += 1;
-        }
+        //   count += 1;
+        // }
 
         [
           onePoint,
@@ -298,13 +406,13 @@ export default {
           sForm,
           threePointsCurve,
         ].forEach((group) => {
-          group.rotation.x = time / 2000;
-          group.rotation.y = time / 1000;
-          group.rotation.z = time / 3000;
+          group.rotation.x = timeDelta / 2;
+          group.rotation.y = timeDelta / 1;
+          group.rotation.z = timeDelta / 3;
 
-          if (changePosition) {
-            group.position.setZ(-count);
-          }
+          // if (changePosition) {
+          //   group.position.setZ(-count);
+          // }
         });
 
         renderer.render(scene, camera);
@@ -318,15 +426,53 @@ export default {
 
       this.updateRendererSize();
     },
+
+    keyupHandler(event) {
+      switch (event.code) {
+        case "KeyW":
+          console.log("Press W");
+          break;
+        case "KeyS":
+          console.log("Press S");
+          break;
+        case "KeyA":
+          console.log("Press A");
+          break;
+        case "KeyF":
+          console.log("Press F");
+          break;
+        case "ArrowUp":
+          console.log("Press Up");
+          break;
+        case "ArrowDown":
+          console.log("Press Down");
+          break;
+        case "ArrowLeft":
+          console.log("Press Left");
+          break;
+        case "ArrowRight":
+          console.log("Press Right");
+          break;
+        case "Space":
+          console.log("Press Space");
+          break;
+        case "KeyP":
+          console.log("Press P");
+          this.isPause = !this.isPause;
+          break;
+      }
+    },
   },
 
   mounted() {
     this.init();
 
     window.addEventListener("resize", this.updateRendererSize());
+    document.addEventListener("keyup", this.keyupHandler);
   },
 
   beforeUnmount() {
     window.removeEventListener("resize", this.updateRendererSize());
+    document.removeEventListener("keyup", this.keyupHandler);
   },
 };
