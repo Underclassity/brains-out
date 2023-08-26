30d7138bf8b9ca1a91c3aaad31dcdb5b71e1caaa
Ivan Harnouski <underclassity@gmail.com>
2023-08-26 16:16:39 +0300

    Fix pit size bug

--- src/components/MainScreen/MainScreen.vue
+++ src/components/MainScreen/MainScreen.vue
@@ -5,7 +5,7 @@
     select.pit-size-select(v-model="pitSize" v-on:change="changePitSize")
         option(value='5x5x12') 5x5x12
         option(value='10x10x12') 10x10x12
-        option(value='7x5x12') 7x4x12
+        option(value='7x4x12') 7x4x12
 </template>
 
 <script src="./MainScreen.js"></script>
