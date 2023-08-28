0ff1028f400fd405f65461067c8e5de664b12f94
Ivan Harnouski <underclassity@gmail.com>
2023-08-28 13:45:12 +0300

    Add menu fade; add reset.css

--- src/components/MenuComponent/MenuComponent.vue
+++ src/components/MenuComponent/MenuComponent.vue
@@ -1,25 +1,26 @@
 <template lang="pug">
-.overlay(v-if="isMenu")
-    .menu
-        h1 Menu
-        hr
+Transition(:duration="{ enter: 500, leave: 800 }")
+    .overlay(v-if="isMenu")
+        .menu
+            h1 Menu
+            hr
 
-        .menu-item
-            span Pit size:
-            select(v-model="currentPitSize" v-on:change="changePitSize")
-                option(value='5x5x12') 5x5x12
-                option(value='10x10x12') 10x10x12
-                option(value='7x4x12') 7x4x12
+            .menu-item
+                span Pit size:
+                select(v-model="currentPitSize" v-on:change="changePitSize")
+                    option(value='5x5x12') 5x5x12
+                    option(value='10x10x12') 10x10x12
+                    option(value='7x4x12') 7x4x12
 
-        .menu-item
-            span Speed:
-            select(v-model="currentSpeed" v-on:change="changeSpeed")
-                option(value=1) 1
-                option(value=2) 2
-                option(value=3) 3
-                option(value=4) 4
+            .menu-item
+                span Speed:
+                select(v-model="currentSpeed" v-on:change="changeSpeed")
+                    option(value=1) 1
+                    option(value=2) 2
+                    option(value=3) 3
+                    option(value=4) 4
 
-        h3.link(v-on:click="closeMenu") Back to game
+            h3.link(v-on:click="closeMenu") Back to game
 </template>
 
 <script src="./MenuComponent.js"></script>
