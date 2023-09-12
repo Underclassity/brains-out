fac629e550b4f5d85a9e95ba5cd0492b77a49429
Ivan Harnouski <underclassity@gmail.com>
2023-09-12 16:04:42 +0300

    Update P6 form

--- src/components/MainScreen/get-random-form.js
+++ src/components/MainScreen/get-random-form.js
@@ -114,7 +114,7 @@ export function getRandomForm() {
     formFunctions[Math.floor(Math.random() * formFunctions.length)];
 
   // Create element
-  const element = formFunction(size, zombieParts, isSimple);
+  const element = formFunctions[4](size, zombieParts, isSimple);
 
   this.restrainElement(element);
 
