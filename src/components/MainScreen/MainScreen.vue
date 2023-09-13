e833933bc30995127f9fa1f8fe34d75a45bc5e28
Ivan Harnouski <underclassity@gmail.com>
2023-09-13 21:56:50 +0300

    Update controls buttons layout

--- src/components/MainScreen/MainScreen.vue
+++ src/components/MainScreen/MainScreen.vue
@@ -6,7 +6,8 @@
         .navigation--item Best Score: {{ maxScore }}
         .navigation--item Score: {{ score }}
 
-    .navigation--item.navigation--menu(v-on:click="openMenu") Menu
+    .navigation--item.navigation--menu(v-on:click="openMenu")
+        span.material-symbols-outlined menu
 
     //- span.menu-button.link.material-symbols-outlined(v-on:click="openMenu") menu
 
@@ -19,33 +20,67 @@
     span.play-button.link.material-symbols-outlined(v-on:click="playCall" v-if="isPause && isDev") play_arrow
 
     .controls(v-show="isControls")
-        .control-item.control-item-left(v-on:click="moveLeft" v-if="isControls")
-            .material-symbols-outlined west
-        .control-item.control-item-right(v-on:click="moveRight" v-if="isControls")
-            .material-symbols-outlined east
+        .controls--columns
+            .controls--column
+                .controls--columns
+                    .controls--column
+                        .controls--button(v-on:click="rotateYMinus")
+                            span -Y
+                        .controls--button(v-on:click="rotateZPlus")
+                            span +Z
+                        .controls--button(v-on:click="rotateYPlus")
+                            span +Y
 
-        .control-item.control-item-top--first(v-on:click="moveUp" v-if="isControls")
-            .material-symbols-outlined north
-        .control-item.control-item-bottom--first(v-on:click="moveDown" v-if="isControls")
-            .material-symbols-outlined south
+                    .controls--column
+                        .controls--button(v-on:click="rotateXMinus")
+                            span -X
+                        .controls--button(v-on:click="rotateZMinus")
+                            span -Z
+                        .controls--button(v-on:click="rotateXPlus")
+                            span +X
 
-        .control-item.control-item-left.control-item-left--first(v-on:click="rotateXMinus" v-if="isControls")
-            .material-symbols-outlined north_west
-        .control-item.control-item-left.control-item-left--last(v-on:click="rotateYMinus" v-if="isControls")
-            .material-symbols-outlined south_west
+            .controls--column.controls--column--buttons
+                .controls--button(v-on:click="moveUp")
+                    span Up
+                .controls--buttons
+                    .controls--button(v-on:click="moveLeft")
+                        span Left
+                    .controls--button(v-on:click="moveRight")
+                        span Right
+                .controls--button(v-on:click="moveDown")
+                    span Down
 
-        .control-item.control-item-right.control-item-right--first(v-on:click="rotateXPlus" v-if="isControls")
-            .material-symbols-outlined north_east
-        .control-item.control-item-right.control-item-right--last(v-on:click="rotateYPlus" v-if="isControls")
-            .material-symbols-outlined south_east
+        .controls--spacebar
+            .controls--button.controls--button--spacebar(v-on:click="drop")
+                span Drop
 
-        .control-item.control-item-top--last(v-on:click="rotateZPlus" v-if="isControls")
-            .material-symbols-outlined north_east
-        .control-item.control-item-bottom--last(v-on:click="rotateZMinus" v-if="isControls")
-            .material-symbols-outlined south_east
+        //- .control-item.control-item-left(v-on:click="moveLeft" v-if="isControls")
+        //-     .material-symbols-outlined west
+        //- .control-item.control-item-right(v-on:click="moveRight" v-if="isControls")
+        //-     .material-symbols-outlined east
 
-        .control-item.control-item--drop(v-on:click="drop" v-if="isControls")
-            .material-symbols-outlined water_drop
+        //- .control-item.control-item-top--first(v-on:click="moveUp" v-if="isControls")
+        //-     .material-symbols-outlined north
+        //- .control-item.control-item-bottom--first(v-on:click="moveDown" v-if="isControls")
+        //-     .material-symbols-outlined south
+
+        //- .control-item.control-item-left.control-item-left--first(v-on:click="rotateXMinus" v-if="isControls")
+        //-     .material-symbols-outlined north_west
+        //- .control-item.control-item-left.control-item-left--last(v-on:click="rotateYMinus" v-if="isControls")
+        //-     .material-symbols-outlined south_west
+
+        //- .control-item.control-item-right.control-item-right--first(v-on:click="rotateXPlus" v-if="isControls")
+        //-     .material-symbols-outlined north_east
+        //- .control-item.control-item-right.control-item-right--last(v-on:click="rotateYPlus" v-if="isControls")
+        //-     .material-symbols-outlined south_east
+
+        //- .control-item.control-item-top--last(v-on:click="rotateZPlus" v-if="isControls")
+        //-     .material-symbols-outlined north_east
+        //- .control-item.control-item-bottom--last(v-on:click="rotateZMinus" v-if="isControls")
+        //-     .material-symbols-outlined south_east
+
+        //- .control-item.control-item--drop(v-on:click="drop" v-if="isControls")
+        //-     .material-symbols-outlined water_drop
 
 //- MenuComponent(
 //-     :isMenu="isMenu"
