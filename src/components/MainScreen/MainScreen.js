22e18aa9d03b0c3c4d52c8e54ff88cd5261846ee
Ivan Harnouski <underclassity@gmail.com>
2023-09-20 21:28:49 +0300

    Move pit levels to separate file

--- src/components/MainScreen/MainScreen.js
+++ src/components/MainScreen/MainScreen.js
@@ -4,13 +4,9 @@ import {
   AmbientLight,
   Audio,
   AudioListener,
-  BoxGeometry,
-  BoxHelper,
   Clock,
   Color,
-  Group,
   MathUtils,
-  Mesh,
   MeshBasicMaterial,
   PerspectiveCamera,
   PointLightHelper,
@@ -64,6 +60,7 @@ import {
   translateHelper,
   setLayerPoint,
 } from "./transform-helpers.js";
+import { initLevelPreview, updateLayersPreview } from "./init-pit-levels.js";
 import { initWaterfall, createElement } from "./waterfall.js";
 import colorPalette from "./color-palette.js";
 import getRandomForm from "./get-random-form.js";
@@ -1580,78 +1577,8 @@ export default {
       return true;
     },
 
-    initLevelPreview() {
-      const { pitDepth, pitWidth, pitHeight, size } = this;
-
-      if (this.pitLevels) {
-        this.scene.remove(this.pitLevels);
-        if (this.pitLevels.dispose) {
-          this.pitLevels.dispose();
-        }
-        this.pitLevels = undefined;
-      }
-
-      this.pitLevels = new Group();
-      this.pitLevels.scale.set(0.5, 0.5, 0.5);
-
-      this.pitLevels.position.set(-pitWidth / 2 - 1, 0, 1);
-
-      const gridColor = new Color(this.gridColor);
-
-      for (let i = 0; i < pitDepth; i++) {
-        const color = new Color(this.colorPalette[i]);
-
-        const boxMaterial = new MeshBasicMaterial({ color });
-        const boxGeometry = new BoxGeometry(1, 1);
-
-        const boxMesh = new Mesh(boxGeometry, boxMaterial);
-
-        boxMesh.position.set(0, -i - size / 2 + pitDepth / 2, 0);
-
-        // Hide all
-        boxMesh.visible = false;
-
-        // Save level for process
-        boxMesh.name = "level";
-        boxMesh.userData.level = i;
-
-        this.pitLevels.add(boxMesh);
-        this.pitLevels.add(new BoxHelper(boxMesh, gridColor));
-      }
-
-      this.scene.add(this.pitLevels);
-
-      return true;
-    },
-
-    updateLayersPreview() {
-      log("Update layers preview");
-
-      const { layers, pitLevels } = this;
-
-      const meshes = pitLevels.children.filter((item) => item.name == "level");
-
-      for (const [index, zLayer] of layers.entries()) {
-        const layerValues = zLayer.reduce((prev, current) => {
-          prev.push(...current);
-
-          return prev;
-        }, []);
-
-        const layerElements = this.layersElements[index].filter((item) => item);
-
-        if (layerValues.includes(1) != layerElements.length > 0) {
-          this.error = `Layer values ${index} not equal to layer elements!`;
-          throw new Error(this.error);
-        }
-
-        const mesh = meshes.find((item) => item.userData.level == index);
-
-        mesh.visible = layerValues.includes(1) ? true : false;
-      }
-
-      return true;
-    },
+    initLevelPreview,
+    updateLayersPreview,
 
     moveToRandomCorner(element) {
       const { pitWidth, pitHeight } = this;
---
+++ src/components/MainScreen/init-pit-levels.js
@@ -0,0 +1,95 @@
+import {
+  BoxGeometry,
+  BoxHelper,
+  Color,
+  Group,
+  Mesh,
+  MeshBasicMaterial,
+} from "three";
+
+/**
+ * Init pit levels preview
+ *
+ * @return  {Boolean}  Result
+ */
+export function initLevelPreview() {
+  const { pitDepth, pitWidth, pitHeight, size } = this;
+
+  if (this.pitLevels) {
+    this.scene.remove(this.pitLevels);
+    if (this.pitLevels.dispose) {
+      this.pitLevels.dispose();
+    }
+    this.pitLevels = undefined;
+  }
+
+  this.pitLevels = new Group();
+  this.pitLevels.scale.set(0.5, 0.5, 0.5);
+
+  this.pitLevels.position.set(-pitWidth / 2 - 1, 0, 1);
+
+  const gridColor = new Color(this.gridColor);
+
+  for (let i = 0; i < pitDepth; i++) {
+    const color = new Color(this.colorPalette[i]);
+
+    const boxMaterial = new MeshBasicMaterial({ color });
+    const boxGeometry = new BoxGeometry(1, 1);
+
+    const boxMesh = new Mesh(boxGeometry, boxMaterial);
+
+    boxMesh.position.set(0, -i - size / 2 + pitDepth / 2, 0);
+
+    // Hide all
+    // boxMesh.visible = false;
+    boxMesh.opacity = 0.1;
+
+    // Save level for process
+    boxMesh.name = "level";
+    boxMesh.userData.level = i;
+
+    this.pitLevels.add(boxMesh);
+    this.pitLevels.add(new BoxHelper(boxMesh, gridColor));
+  }
+
+  this.scene.add(this.pitLevels);
+
+  return true;
+}
+
+/**
+ * Update pit levels preview
+ *
+ * @return  {Boolean}  Result
+ */
+export function updateLayersPreview() {
+  log("Update layers preview");
+
+  const { layers, pitLevels } = this;
+
+  const meshes = pitLevels.children.filter((item) => item.name == "level");
+
+  for (const [index, zLayer] of layers.entries()) {
+    const layerValues = zLayer.reduce((prev, current) => {
+      prev.push(...current);
+
+      return prev;
+    }, []);
+
+    const layerElements = this.layersElements[index].filter((item) => item);
+
+    if (layerValues.includes(1) != layerElements.length > 0) {
+      this.error = `Layer values ${index} not equal to layer elements!`;
+      throw new Error(this.error);
+    }
+
+    const mesh = meshes.find((item) => item.userData.level == index);
+
+    // mesh.visible = layerValues.includes(1) ? true : false;
+    mesh.opacity = layerValues.includes(1) ? 1 : 0.1;
+  }
+
+  return true;
+}
+
+export default initLevelPreview;
