e64fb1f17806e4f81b64d01ec7a2d5eb2d13fe08
Ivan Harnouski <underclassity@gmail.com>
2023-09-26 15:22:17 +0300

    Update pit levels color palette

--- src/components/MainScreen/color-palette.js
+++ src/components/MainScreen/color-palette.js
@@ -7,46 +7,59 @@ import { Color } from "three";
  */
 export const colorPalette = [
   // https://www.color-hex.com/color-palette/33364
-  new Color("rgb(0, 223, 37)"),
-  new Color("rgb(9, 0, 255)"),
-  new Color("rgb(7, 3, 94)"),
-  new Color("rgb(253, 39, 255)"),
-  new Color("rgb(107, 23, 180)"),
+  // new Color("rgb(0, 223, 37)"),
+  // new Color("rgb(9, 0, 255)"),
+  // new Color("rgb(7, 3, 94)"),
+  // new Color("rgb(253, 39, 255)"),
+  // new Color("rgb(107, 23, 180)"),
 
   // https://www.color-hex.com/color-palette/21598
   // new Color("rgb(223, 169, 0)"),
-  new Color("rgb(255, 255, 243)"),
-  new Color("rgb(124, 255, 0)"),
-  new Color("rgb(146, 145, 142)"),
-  new Color("rgb(164, 236, 238)"),
+  // new Color("rgb(255, 255, 243)"),
+  // new Color("rgb(124, 255, 0)"),
+  // new Color("rgb(146, 145, 142)"),
+  // new Color("rgb(164, 236, 238)"),
 
   // https://www.color-hex.com/color-palette/46469
-  new Color("rgb(159, 20, 20)"),
-  new Color("rgb(71, 30, 30)"),
-  // new Color("rgb(246, 11, 11)"),
-  new Color("rgb(15, 144, 23)"),
-  new Color("rgb(167, 11, 186)"),
+  // new Color("rgb(159, 20, 20)"),
+  // new Color("rgb(71, 30, 30)"),
+  // // new Color("rgb(246, 11, 11)"),
+  // new Color("rgb(15, 144, 23)"),
+  // new Color("rgb(167, 11, 186)"),
 
   // https://www.color-hex.com/color-palette/28861
-  new Color("rgb(92, 141, 113)"),
-  new Color("rgb(21, 103, 19)"),
-  new Color("rgb(116, 94, 94)"),
-  new Color("rgb(137, 55, 55)"),
-  new Color("rgb(13, 49, 62)"),
+  // new Color("rgb(92, 141, 113)"),
+  // new Color("rgb(21, 103, 19)"),
+  // new Color("rgb(116, 94, 94)"),
+  // new Color("rgb(137, 55, 55)"),
+  // new Color("rgb(13, 49, 62)"),
 
   // https://www.color-hex.com/color-palette/114259
-  new Color("rgb(170, 108, 10)"),
-  new Color("rgb(0, 0, 0)"),
-  new Color("rgb(143, 0, 0)"),
-  new Color("rgb(39, 78, 19)"),
-  new Color("rgb(91, 91, 91)"),
+  // new Color("rgb(170, 108, 10)"),
+  // new Color("rgb(0, 0, 0)"),
+  // new Color("rgb(143, 0, 0)"),
+  // new Color("rgb(39, 78, 19)"),
+  // new Color("rgb(91, 91, 91)"),
 
   // https://www.color-hex.com/color-palette/114260
-  new Color("rgb(255, 255, 255)"),
-  new Color("rgb(226, 206, 5)"),
-  new Color("rgb(0, 65, 123)"),
-  new Color("rgb(214, 109, 0)"),
-  new Color("rgb(170, 170, 170)"),
+  // new Color("rgb(255, 255, 255)"),
+  // new Color("rgb(226, 206, 5)"),
+  // new Color("rgb(0, 65, 123)"),
+  // new Color("rgb(214, 109, 0)"),
+  // new Color("rgb(170, 170, 170)"),
+
+  new Color("#F5083A"),
+  new Color("#E9353A"),
+  new Color("#D85C39"),
+  new Color("#C17637"),
+  new Color("#A58A36"),
+  new Color("#829B34"),
+  new Color("#669B38"),
+  new Color("#5C8C3F"),
+  new Color("#517A46"),
+  new Color("#44644C"),
+  new Color("#324551"),
+  new Color("#282F54"),
 ];
 
 /**
