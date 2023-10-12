a269200307c1137f2bfccbf02944254a3facd338
Ivan Harnouski <underclassity@gmail.com>
2023-10-12 22:39:16 +0300

    Comment back R press for random rotate

--- src/components/MainScreen/MainScreen.js
+++ src/components/MainScreen/MainScreen.js
@@ -2749,12 +2749,12 @@ export default {
             this.openMenu();
           }
           break;
-        // case "KeyR":
-        //   if (!this.isMenu) {
-        //     this.movesCounter += 2;
-        //     this.randomRotate(3);
-        //   }
-        //   break;
+        case "KeyR":
+          if (!this.isMenu) {
+            this.movesCounter += 2;
+            this.randomRotate(3);
+          }
+          break;
       }
     },
 
