9511890c098ae8335a0693735f59d6a204bd7a94
Ivan Harnouski <underclassity@gmail.com>
2023-11-01 00:18:12 +0300

    Fix colorize performance

--- src/components/MainScreen/colorize-element.js
+++ src/components/MainScreen/colorize-element.js
@@ -56,39 +56,43 @@ export function colorizeElement(element, layer) {
       material = new MeshBasicMaterial({ color });
       material.name = `color-old-${color.getHexString()}`;
       material.needsUpdate = true;
+
       return material;
     }
 
-    material.color.set(color);
-
-    if (atlas) {
-      material.map = atlas;
-    }
-
-    material.name = `color-${color.getHexString()}-${index}`;
+    material = new MeshBasicMaterial({ color, map: atlas });
+    material.name = `color-old-${color.getHexString()}`;
     material.needsUpdate = true;
 
     return material;
+
+    // material.color.set(color);
+
+    // if (atlas) {
+    //   material.map = atlas;
+    // }
+
+    // material.name = `color-${color.getHexString()}-${index}`;
+    // material.needsUpdate = true;
+
+    // return material;
   };
 
-  element.traverse((obj) => {
-    if (!obj.isMesh) {
-      return false;
-    }
+  element.material = colorize(element.material);
 
-    if (this.isOldColorize) {
-      obj.material = colorize(obj.material);
-      return true;
-    }
+  // element.traverse((obj) => {
+  //   if (!obj.isMesh) {
+  //     return false;
+  //   }
 
-    if (Array.isArray(obj.material)) {
-      obj.material = obj.material.map(colorize);
+  //   if (Array.isArray(obj.material)) {
+  //     obj.material = obj.material.map(colorize);
 
-      return false;
-    }
+  //     return false;
+  //   }
 
-    obj.material = colorize(obj.material);
-  });
+  //   obj.material = colorize(obj.material);
+  // });
 
   return true;
 }
